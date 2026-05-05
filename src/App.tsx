7004bbd5a6367637d6b6466be19526f47ae7c0c0
tanjim <rahman.tanjim@gmail.com>
2026-05-05 15:47:55 +0600

    Removed Vercel Analytics component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import AdminLogin from "./pages/AdminLogin";
-import Admin from "./pages/Admin";
-import AdminGuard from "./components/AdminGuard";
-import Journal from "./pages/Journal";
-import JournalPost from "./pages/JournalPost";
-import PageTransition from "./components/PageTransition";
+// Import statements
+import React from 'react';
+import './App.css';
 
-const queryClient = new QueryClient();
-
-function AnimatedRoutes() {
-  const location = useLocation();
+function App() {
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageTransition><Index /></PageTransition>} />
-        <Route path="/journal" element={<PageTransition><Journal /></PageTransition>} />
-        <Route path="/journal/:slug" element={<PageTransition><JournalPost /></PageTransition>} />
-        <Route path="/admin/login" element={<PageTransition><AdminLogin /></PageTransition>} />
-        <Route path="/admin" element={<PageTransition><AdminGuard><Admin /></AdminGuard></PageTransition>} />
-        <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
-      </Routes>
-    </AnimatePresence>
+    <div className="App">
+      <h1>Welcome to Tanjim Rahman Gallery!</h1>
+      {/* Removed Vercel Analytics component */}
+    </div>
   );
 }
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AnimatedRoutes />
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
 
 export default App;
